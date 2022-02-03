d7ae488c115172a7e6f84f10ae0575836549aaf7
Piernes <piernesjose@gmail.com>
2022-02-03 11:13:59 +0700

    Add project files.

---
+++ Stikiwebapp/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace Stikiwebapp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sDMSTIKIToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sISTERToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sIMAKToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bPJSTKToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.wEBSTIKIToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zOOMKULIAHBUKANS1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zOOMKULIAHS1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.oJSSTIKIToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pERPUSTAKAANToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(502, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sDMSTIKIToolStripMenuItem,
+            this.sISTERToolStripMenuItem,
+            this.sIMAKToolStripMenuItem,
+            this.bPJSTKToolStripMenuItem,
+            this.wEBSTIKIToolStripMenuItem,
+            this.zOOMKULIAHBUKANS1ToolStripMenuItem,
+            this.zOOMKULIAHS1ToolStripMenuItem,
+            this.oJSSTIKIToolStripMenuItem,
+            this.pERPUSTAKAANToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.menuToolStripMenuItem.Text = "Menu";
+            // 
+            // sDMSTIKIToolStripMenuItem
+            // 
+            this.sDMSTIKIToolStripMenuItem.Name = "sDMSTIKIToolStripMenuItem";
+            this.sDMSTIKIToolStripMenuItem.Size = new System.Drawing.Size(219, 22);
+            this.sDMSTIKIToolStripMenuItem.Text = "SDM STIKI";
+            // 
+            // sISTERToolStripMenuItem
+            // 
+            this.sISTERToolStripMenuItem.Name = "sISTERToolStripMenuItem";
+            this.sISTERToolStripMenuItem.Size = new System.Drawing.Size(219, 22);
+            this.sISTERToolStripMenuItem.Text = "SISTER";
+            // 
+            // sIMAKToolStripMenuItem
+            // 
+            this.sIMAKToolStripMenuItem.Name = "sIMAKToolStripMenuItem";
+            this.sIMAKToolStripMenuItem.Size = new System.Drawing.Size(219, 22);
+            this.sIMAKToolStripMenuItem.Text = "SIMAK";
+            // 
+            // bPJSTKToolStripMenuItem
+            // 
+            this.bPJSTKToolStripMenuItem.Name = "bPJSTKToolStripMenuItem";
+            this.bPJSTKToolStripMenuItem.Size = new System.Drawing.Size(219, 22);
+            this.bPJSTKToolStripMenuItem.Text = "BPJS TK";
+            // 
+            // wEBSTIKIToolStripMenuItem
+            // 
+            this.wEBSTIKIToolStripMenuItem.Name = "wEBSTIKIToolStripMenuItem";
+            this.wEBSTIKIToolStripMenuItem.Size = new System.Drawing.Size(219, 22);
+            this.wEBSTIKIToolStripMenuItem.Text = "WEB STIKI";
+            // 
+            // zOOMKULIAHBUKANS1ToolStripMenuItem
+            // 
+            this.zOOMKULIAHBUKANS1ToolStripMenuItem.Name = "zOOMKULIAHBUKANS1ToolStripMenuItem";
+            this.zOOMKULIAHBUKANS1ToolStripMenuItem.Size = new System.Drawing.Size(219, 22);
+            this.zOOMKULIAHBUKANS1ToolStripMenuItem.Text = "ZOOM KULIAH (BUKAN S1)";
+            // 
+            // zOOMKULIAHS1ToolStripMenuItem
+            // 
+            this.zOOMKULIAHS1ToolStripMenuItem.Name = "zOOMKULIAHS1ToolStripMenuItem";
+            this.zOOMKULIAHS1ToolStripMenuItem.Size = new System.Drawing.Size(219, 22);
+            this.zOOMKULIAHS1ToolStripMenuItem.Text = "ZOOM KULIAH S1";
+            // 
+            // oJSSTIKIToolStripMenuItem
+            // 
+            this.oJSSTIKIToolStripMenuItem.Name = "oJSSTIKIToolStripMenuItem";
+            this.oJSSTIKIToolStripMenuItem.Size = new System.Drawing.Size(219, 22);
+            this.oJSSTIKIToolStripMenuItem.Text = "OJS STIKI";
+            // 
+            // pERPUSTAKAANToolStripMenuItem
+            // 
+            this.pERPUSTAKAANToolStripMenuItem.Name = "pERPUSTAKAANToolStripMenuItem";
+            this.pERPUSTAKAANToolStripMenuItem.Size = new System.Drawing.Size(219, 22);
+            this.pERPUSTAKAANToolStripMenuItem.Text = "PERPUSTAKAAN";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(502, 178);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sDMSTIKIToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sISTERToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sIMAKToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem bPJSTKToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem wEBSTIKIToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zOOMKULIAHBUKANS1ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zOOMKULIAHS1ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem oJSSTIKIToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pERPUSTAKAANToolStripMenuItem;
+    }
+}
+
